d111330f1dc260c07656d6f05962c6d8e706a44d
Nicolas Sapaya <nicolas.sapaya@gmail.com>
2017-06-21 22:22:52 -0300

    Metodos

--- src/colorear/GrafoNDNP.java
+++ src/colorear/GrafoNDNP.java
@@ -4,7 +4,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
+import generadores.Nodo;
 import matrizSimetrica.MatrizSimetrica;
 
 public class GrafoNDNP {
@@ -17,9 +19,9 @@ public class GrafoNDNP {
 	private int cantAristas;
 	private double adyacencia;
 	private int[] listaColoreada;
-	private ArrayList<Integer> listaNodos = new ArrayList<Integer>();
+	private ArrayList<Nodo> listaNodos = new ArrayList<Nodo>();
 	private int[] frecuencia;
-	private int mejorRep;
+	private int mejorRepeticion;
 
 	public GrafoNDNP(String path) throws FileNotFoundException {
 		this.matriz = new MatrizSimetrica(path);
@@ -36,7 +38,8 @@ public class GrafoNDNP {
 		this.cantColores = 1;
 
 		for (int i = 0; i < cantNodos; i++) {
-			listaNodos.add(i);
+			listaNodos.add(new Nodo(i));
+			listaNodos.get(i).setGrado(grados[i]);
 		}	
 	}
 	
@@ -44,12 +47,31 @@ public class GrafoNDNP {
 		String path = "ColorearGrafo8-3.in";	
 //		String path = "ColorearCirculo8-2.in";
 		GrafoNDNP grafo = new GrafoNDNP(path);
-		grafo.mezclar();
-		grafo.colorear();
+		grafo.secuencial(3);
 	}
+	
 	public void mezclar() {
 		Collections.shuffle(listaNodos);
 	}
+	
+	private void ordenarPorGrado(int i) {
+		if (i == 1) {
+			Collections.sort(listaNodos, new Comparator<Nodo>(){
+				@Override
+				public int compare(Nodo n1, Nodo n2) {
+					return n1.getGrado() - n2.getGrado();
+				}
+			});		
+		} else {
+			Collections.sort(listaNodos, new Comparator<Nodo>(){
+				@Override
+				public int compare(Nodo n1, Nodo n2) {
+					return n2.getGrado() - n1.getGrado();
+				}
+			});	
+		}
+	}	
+	
 	
 	public void colorear() {
 		this.cantColores = 1;
@@ -61,27 +83,113 @@ public class GrafoNDNP {
 			listaColoreada[i] = noColoreado;
 		}
 		// El primero lo pinto por defecto
-		listaColoreada[listaNodos.get(0)] = color;
+		listaColoreada[listaNodos.get(0).getIdNodo()] = color;
 		// Ahora empiezo por el que sigue y voy preguntando
 		for (int i = 1; i < cantNodos; i++) {
-			listaColoreada[listaNodos.get(i)] = color;
+			listaColoreada[listaNodos.get(i).getIdNodo()] = color;
 			j = 0;
 			while (j < cantNodos) {
-				if (matriz.getValor(listaNodos.get(i), j) && listaColoreada[listaNodos.get(i)] == listaColoreada[j]) {
+				if (matriz.getValor(listaNodos.get(i).getIdNodo(), j) && listaColoreada[listaNodos.get(i).getIdNodo()] == listaColoreada[j]) {
 					color++;
 					if (color > cantColores) {
 						cantColores = color;						
 					}
-					listaColoreada[listaNodos.get(i)] = color;
+					listaColoreada[listaNodos.get(i).getIdNodo()] = color;
 					j = -1;
 				}
 				j++;
 			}
 			color = colorBase;
 		}
+		System.out.println("Me va tirando por repeticion, esto esta en colorear()");
 		for (int i = 0; i < cantNodos; i++) {
 			System.out.println(i + " Color: " + listaColoreada[i]);
 		}
+	}
+	
+	public void secuencial(int repeticiones) {
+		int [] mejorColoreado = new int[cantNodos];
+		int mejorCantColores = 0;
+		
+		for(int i = 0; i < repeticiones; i++) {
+			System.out.println("Repeticion: " + i);
+			mezclar();
+			colorear();
+			System.out.println("Colores de la repeticion: " + cantColores);
+			//Guardar mejor resultado	
+			if(mejorCantColores == 0 || mejorCantColores > this.cantColores) {
+				mejorColoreado = listaColoreada.clone();
+				mejorCantColores = cantColores;
+				mejorRepeticion = i;
+			}
+			//guardo resultado en la tabla de frecuencia
+			frecuencia[cantColores-1]++;
+		}
+		//me quedo con el mejor resultado
+		listaColoreada = mejorColoreado.clone();
+		cantColores = mejorCantColores;
+		System.out.println("MEJOR CANT DE COLORES: " + cantColores);
+		System.out.println("MEJOR LISTA DE COLORES");
+		for (int i = 0; i < listaColoreada.length; i++) {
+			System.out.println(i + " Color: " + listaColoreada[i]);
+		}
+		System.out.println("FRECUENCIAS");
+		for (int i = 0; i < frecuencia.length; i++) {
+			System.out.println("Color: " + Integer.valueOf(i+1) + " Frecuencia :" + frecuencia[i]);
+		}
+		System.out.println("MEJOR REPETICION: " + mejorRepeticion);
+	}
+
+	public void matula(int repeticiones) {
+		int [] mejorColoreado = new int[cantNodos];
+		int mejorCantColores = 0;
+		
+		for(int i = 0; i < repeticiones; i++) {
+			
+			mezclar();
+			ordenarPorGrado(1); //le paso un 1 porque es creciente
+			colorear();
+			
+			//Guardar el mejor resultado	
+			if(mejorCantColores == 0 || mejorCantColores > this.cantColores) {
+				mejorColoreado = listaColoreada.clone();
+				mejorCantColores = cantColores;
+				mejorRepeticion = i;
+			}
+			//guardo resultado en la tabla de frecuencia
+			frecuencia[cantColores-1]++;
+		}
+		//me quedo con el mejor resultado
+		listaColoreada = mejorColoreado.clone();
+		cantColores = mejorCantColores;
+	}
+
+	public void wellshPowell(int repeticiones) {
+		int [] mejorColoreado = new int[cantNodos];
+		int mejorCantColores = 0;
+		
+		for(int i = 0; i < repeticiones; i++) {
+			
+			mezclar();
+			ordenarPorGrado(0); //le paso un 0 porque es decreciente
+			colorear();
+			
+			//Guardar el mejor resultado	
+			if(mejorCantColores == 0 || mejorCantColores > this.cantColores) {
+				mejorColoreado = listaColoreada.clone();
+				mejorCantColores = cantColores;
+				mejorRepeticion = i;
+			}
+			//guardo resultado en la tabla de frecuencia
+			frecuencia[cantColores-1]++;
+		}
+		//me quedo con el mejor resultado
+		listaColoreada = mejorColoreado.clone();
+		cantColores = mejorCantColores;
+	}
+
+	public void imprimir() {
+
 	}
 	
 	// COLOREO PABLO
@@ -177,29 +285,4 @@ public class GrafoNDNP {
 //			nodoAnt = nodo1;
 //		}
 //	}
-	
-	public void secuencial() {
-		// PINTAR EL PRIMERO // 0
-//		colorear(0);
-//		for (int i = 0; i < matriz.getCantNodos(); i++) {
-//			for (int j = i + 1; j < matriz.getCantNodos(); j++) {
-//				// SI ESTAN UNIDOS AL OTRO LO PINTO DE OTRO COLOR
-//				if (matriz.getValor(i, j)) {
-//					colorear(j);
-//				}
-//			}
-//		}
-	}
-
-	public void matula() {
-
-	}
-
-	public void wellshPowell() {
-
-	}
-
-	public void imprimir() {
-
-	}
 }
